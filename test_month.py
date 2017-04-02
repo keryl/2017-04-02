2b2243e3548192f72575dd8a29f7930a3dd7db0e
keryl <kerylklonye@gmail.com>
2017-04-02 15:18:36 +0300

    implement test_month file

--- test_month.py
+++ test_month.py
@@ -9,6 +9,14 @@ class TestMonth(unittest.TestCase):
         self.assertEqual('False', is_month('Mon'))
         self.assertEqual('False, is_month('Five'))
 
+    def test_it_does_not_return_value_for_non_strings(self):
+        self.assertEqual('argument should be of type string', is_month(1))
+        self.assertEqual('argument should be of type string', is_month(True))
+        self.assertEqual('argument should be of type string', is_month([]))
+        self.assertEqual('argument should be of type string', is_month({}))
+
+
+
 
 if __name__=="__main__":
     unittest.main()
