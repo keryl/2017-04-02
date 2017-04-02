4853434c36923a7c930f12d1159e504ed5bc6f2b
keryl <kerylklonye@gmail.com>
2017-04-02 16:51:20 +0300

    implement month file

--- month.py
+++ month.py
@@ -1,6 +1,19 @@
-def is_month(str):
-    month = [jan, feb, mar, apr, may, jun, jul, aug, sept, oct, nov, dec]
-    if type(str) == month:
+def is_month(month):
+
+    # first check given month is string
+    if type(month) != str:
+        return "argument should be of type string"
+
+    months = ['jan', 'feb', 'mar', 'apr', 'may', 'jun', 'jul', 'aug', 'sep', 'oct', 'nov', 'dec']
+
+    # return True if given month is in months list
+    if month in months:
         return True
+
+    # else return false
     else:
-        return "False"
+        return False
+
+if __name__=="__main__":
+    jan_is_month = is_month("jan")
+    print ("jan is a month: " + jan_is_month)
--- test_month.py
+++ test_month.py
@@ -1,22 +1,20 @@
 import unittest
-import month
+from month import is_month
 
 class TestMonth(unittest.TestCase):
 
     def test_it_returns_true_if_argument_is_a_month(self):
-        self.assertEqual('True, is_month('Jan'))
-        self.assertEqual('True', is_month('Nov'))
-        self.assertEqual('False', is_month('Mon'))
-        self.assertEqual('False, is_month('Five'))
+        self.assertEqual(True, is_month('jan'))
+        self.assertEqual(True, is_month('nov'))
+        self.assertEqual(False, is_month('Mon'))
+        self.assertEqual(False, is_month('Five'))
 
     def test_it_does_not_return_value_for_non_strings(self):
         self.assertEqual('argument should be of type string', is_month(1))
         self.assertEqual('argument should be of type string', is_month(True))
         self.assertEqual('argument should be of type string', is_month([]))
         self.assertEqual('argument should be of type string', is_month({}))
-
-
-
+        self.assertNotEqual('argument should be of type string', is_month('test'))
 
 if __name__=="__main__":
     unittest.main()
